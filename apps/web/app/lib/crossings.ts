c39336e6f427d2df232e9becfb84f0b917863831
grospitz-wreck-it <grospitz@gmail.com>
2026-07-01 06:34:10 +0000

    loehne und coe

--- apps/web/app/lib/crossings.ts
+++ apps/web/app/lib/crossings.ts
@@ -1,17 +1,41 @@
 export const crossings = {
   kirchlengern: {
     id: "6097",
+    name: "Kirchlengern",
+    eva: "8003288",
+    warningSeconds: 60,
+    closedSeconds: 90,
+  },
 
-    name:
-      "Kirchlengern",
+  buende: {
+    id: "6098",
+    name: "Bünde(Westf)",
+    eva: "8000059",
+    warningSeconds: 60,
+    closedSeconds: 90,
+  },
 
-    eva:
-      "8003288",
+  herford: {
+    id: "6099",
+    name: "Herford",
+    eva: "8000162",
+    warningSeconds: 60,
+    closedSeconds: 90,
+  },
 
-    warningSeconds:
-      60,
+  loehne: {
+    id: "6100",
+    name: "Löhne(Westf)",
+    eva: "8000233",
+    warningSeconds: 60,
+    closedSeconds: 90,
+  },
 
-    closedSeconds:
-      90,
+  minden: {
+    id: "6101",
+    name: "Minden(Westf)",
+    eva: "8000252",
+    warningSeconds: 60,
+    closedSeconds: 90,
   },
 };
