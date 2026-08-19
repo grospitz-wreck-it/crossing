c881825926f2157119c67da3cf9eecd84bbfe6f5
grospitz-wreck-it <grospitz@gmail.com>
2026-08-19 20:38:00 +0200

    Correct Mobilithek diagnostics workspace import

--- apps/web/app/api/mobilithek/diagnostics/route.ts
+++ apps/web/app/api/mobilithek/diagnostics/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getMobilithekTrainDiagnostics } from "../../../../../packages/db-api-client/src/mobilithekTimetable";
+import { getMobilithekTrainDiagnostics } from "../../../../../../packages/db-api-client/src/mobilithekTimetable";
 
 export const dynamic = "force-dynamic";
 export const runtime = "nodejs";
