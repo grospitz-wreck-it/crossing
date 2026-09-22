0abd2839c646f9d24170635b6c4a8fc4cac3e0e9
grospitz-wreck-it <grospitz@gmail.com>
2026-09-22 12:57:13 +0200

    fix: request relay diagnostic mode explicitly

--- workers/mobilithek-refresh/src/cloudflareMobilithek.ts
+++ workers/mobilithek-refresh/src/cloudflareMobilithek.ts
@@ -94,7 +94,7 @@ export async function fetchRelayDiagnostics(
       headers: {
         authorization: `Bearer ${relayToken}`,
         "content-type": "application/json",
-        accept: "application/json",
+        accept: "application/json",\n        "x-mobilithek-diagnostic": "1",
       },
       body: JSON.stringify({
         subscriptionId,
